3dc7b30ff0bfdafea703b195d31a1e56cf7be699
Erik Ronström <erik.ronstrom@doremir.com>
2016-10-20 16:52:33 +0200

    SVG incipit creation script

---
+++ bin/svg-incipit.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+my $LineElements = 0;
+my $OffsetX;
+my $OffsetY;
+
+my $Head;
+my $Output = '';
+
+my $Scale = 0.4;
+
+
+while (my $Line = <>) {
+    if ($Line =~ /<svg /) {
+        $Head = $Line;
+        next;
+    }
+    if (($Line =~ /<line .*x1="([\-\d\.]+)".*x2="([\-\d\.]+)"/) and ($2 > ($1 + 50))) {
+        $LineElements++;
+        if ($LineElements == 5) {
+             $Line =~ /translate\(([0-9\.]+), ([0-9\.]+)\)/;
+             $OffsetX = $1;
+             $OffsetY = $2;
+        }
+        last if $LineElements > 5;
+    }
+    $Output .= $Line;
+}
+
+$OffsetY -= 4;
+
+$OffsetX = -$OffsetX;
+$OffsetY = -$OffsetY;
+
+#$OffsetX *= $Scale;
+#$OffsetY *= $Scale;
+
+$Head =~ s/width="(.*?)"/width="50mm"/;
+$Head =~ s/height="(.*?)"/height="10mm"/;
+$Head =~ s/viewBox="(.*?)"/viewBox="0 0 65 12.5"/;
+
+print $Head;
+print "<g transform=\"translate($OffsetX, $OffsetY)\">\n\n";
+print $Output;
+print "</g></svg>\n";
