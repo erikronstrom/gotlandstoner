2cee00a285e550d5868f182db71c102e6a6355d2
Erik Ronström <erik.ronstrom@doremir.com>
2016-12-28 23:52:37 +0100

    Hyphenation

--- include/hyphenation.pm
+++ include/hyphenation.pm
@@ -8,15 +8,24 @@ use utf8;
 @Hyphenation::Words = (
     "an-to-go",
     "an-mod-ad",
+    "brud-huset",
+    "bröllops-gäster",
     "bröllops-hög-tid-lig-heterna",
+    "bröl-lops-marscher",
+    "bud-ande",
     "dans-till-ställ-ningar",
     "fämtio-riks-dalers-sedel",
     "Grodda-karl-ar-na",
     "hop-lägg-nings-ka-la-ser",
+    "hög-tidlig",
+    "rid-marsch-erna",
     "Stånds-per-so-ner",
+    "Spel-mannen",
     "spel-mans-släkt",
+    "spel-männen",
     "vete-mjöl",
-    "åter-vägen"
+    "åter-vägen",
+    "över-ens-stämma"
 );
 
 
