1b9cb6c48aa0bf6e2434aab6970b4671dd4ee9c5
Erik Ronström <erik.ronstrom@doremir.com>
2017-06-16 21:21:33 +0200

    Remove fredin.out when generating books, to avoid TOC issues (not sure it works, though)

--- bin/make-books.sh
+++ bin/make-books.sh
@@ -4,6 +4,9 @@ if [ -z $1 ]
 then
   VERBOSE=-interaction=batchmode
 fi
+rm -f fredin.out
 bin/generate-book-2.pl book 2 && lualatex $VERBOSE book/fredin.latex && mv fredin.pdf bok2.pdf
+rm fredin.out
 bin/generate-book-2.pl book 3 && lualatex $VERBOSE book/fredin.latex && mv fredin.pdf bok3.pdf
+rm fredin.out
 echo
